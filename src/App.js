4ddabe484f091a2d6af0f1a56c731e2657975c0b
BrunoHernandezE <launchx06540@innovaccion.mx>
2023-06-05 23:59:29 -0600

    almost initial code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Import routes and router
+import { Routes, Route } from "react-router-dom";
+
+// Components
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+const App = () => {
+  return <div>react app</div>;
+};
+
+export default App;
